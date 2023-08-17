215b46ff5a5015674d47347032853a6764cd9fcf
vaidehi <vaidehi117v@gmail.com>
2023-08-16 20:48:45 -0400

    finch model

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Finch
 
 # Create your views here.
 def home(request):
@@ -6,3 +7,8 @@ def home(request):
 
 def about(request): 
     return render(request, 'about.html')
+
+def finchs_index(request):
+    return render(request, 'finchs/index.html', {
+        'finchs': finchs
+    })
